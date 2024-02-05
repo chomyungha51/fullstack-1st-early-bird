2844ad43447913ec6ce9ae4dacadb948c0732c8d
yjlee0235 <yjlee0235@naver.com>
2024-02-05 16:10:35 +0900

    Fix: 에러 처리 추가

--- backend/api/src/main/java/com/earlybird/api/ticket/controller/TicketController3.java
+++ backend/api/src/main/java/com/earlybird/api/ticket/controller/TicketController3.java
@@ -3,11 +3,13 @@ package com.earlybird.api.ticket.controller;
 import com.earlybird.api.ticket.dto.TicketUseResponse;
 import com.earlybird.api.ticket.service.TicketService3;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/tickets")
@@ -16,8 +18,12 @@ public class TicketController3 {
 
     @PatchMapping("/{ticketId}")
     public TicketUseResponse useTicket(@PathVariable Long ticketId) {
-        ticketService3.useTicket(ticketId);
-
-        return new TicketUseResponse("success");
+        try {
+            ticketService3.useTicket(ticketId);
+            return new TicketUseResponse("success");
+        } catch (Exception e) {
+            log.error("Exception occured", e);
+            return new TicketUseResponse("fail");
+        }
     }
 }
