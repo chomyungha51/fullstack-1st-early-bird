8c6bb742f0d5ec2c9d42c7954d39dc2ad5e6994a
jikwan <zc149@naver.com>
2024-02-06 12:19:56 +0900

    login front

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,29 +1,74 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import DefaultLayout, { notify } from "../layouts/DefaultLayout";
+import { useState } from "react";
 
-const Login = () => {
-  // TODO : 아이디 비밀번호 입력받기
 
-  const login = () => {
-    // TODO : 로그인 api 호출 및 response에 따른 처리
-    notify("로그인 성공/실패");
+const Login = ({ user, onChangeUser }) => {
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [inputCheck, setInputCheck] = useState(false);
+
+  const login = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: id,
+          password: password
+        })
+      });
+
+      const data = await response.json();
+
+      if (data.status === 'success') {
+        // onChangeUser(data);
+
+        navigate("/", { state: data });
+
+        console.log(data);
+      } else if (data.status === 'fail') {
+        setInputCheck(true);
+
+        setTimeout(() => {
+          setInputCheck(false)
+        }, 1000)
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+
   return (
     <>
       <DefaultLayout>
-        <h1 className="text-red-700">Login</h1>
-        <Link to="/"> 홈 화면으로 이동 </Link>
-        <div>
-          <div>
-            아이디
-            <input />
-          </div>
-          <div>
-            비밀번호
-            <input />
-          </div>
-          <button onClick={login}>로그인</button>
+        <div className="flex justify-center">
+          <img src="src/assets/logo.png"
+            className=""
+            width={300}
+            height={300} />
         </div>
+        <Link className="flex justify-center" to="/">
+          홈 화면으로 이동 </Link>
+        <div className="flex justify-center ">
+          <span className="inline-flex flex-col mt-10  bg-yellow-300">
+            <span className="inline-flex">
+              <img className="bg-yellow-400" src="src/assets/pwd-icon.png" width={30} height={10} />
+              <input className="bg-yellow-200 text-black" id='id' value={id} onChange={event => setId(event.target.value)} />
+            </span>
+            <span className="inline-flex mt-0.5">
+              <img className="bg-yellow-400" src="src/assets/id-icon.png" width={30} height={10} />
+              <input className="bg-yellow-200 text-black" id='password' type="password" value={password} onChange={event => setPassword(event.target.value)} />
+            </span>
+            <button className="text-black" onClick={login}>로그인</button>
+          </span>
+        </div>
+        {inputCheck && <p className="flex justify-center">확인 해주세요</p>}
       </DefaultLayout>
     </>
   );
