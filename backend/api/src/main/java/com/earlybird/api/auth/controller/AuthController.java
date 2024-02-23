b83d1c2a9cff141e207b91c68bd72a2fb331b660
yjlee0235 <yjlee0235@naver.com>
2024-02-23 11:19:20 +0900

    CORS 설정 제거

--- backend/api/src/main/java/com/earlybird/api/auth/controller/AuthController.java
+++ backend/api/src/main/java/com/earlybird/api/auth/controller/AuthController.java
@@ -6,7 +6,10 @@ import com.earlybird.api.auth.dto.AuthLoginResponse;
 import com.earlybird.api.auth.service.AccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,7 +19,6 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-@CrossOrigin(origins="http://localhost:8081")
 public class AuthController {
     private final AccountService accountService;
     private final ObjectMapper objectMapper;
--- backend/api/src/main/java/com/earlybird/api/user/controller/UserController.java
+++ backend/api/src/main/java/com/earlybird/api/user/controller/UserController.java
@@ -4,14 +4,16 @@ import com.earlybird.api.user.domain.User;
 import com.earlybird.api.user.dto.SearchUserResponse;
 import com.earlybird.api.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
-@CrossOrigin(origins="http://localhost:8081")
 public class UserController {
     private final UserService userService;
 
